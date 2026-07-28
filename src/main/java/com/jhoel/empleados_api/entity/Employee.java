509fcf3fb7b9d1118711c37559daed7b6492e46b
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-07-28 13:08:35 -0500

    feat: create employee entity

---
+++ src/main/java/com/jhoel/empleados_api/entity/Employee.java
@@ -0,0 +1,38 @@
+package com.jhoel.empleados_api.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "employee")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "birth_date", nullable = false)
+    private LocalDate birthDate;
+
+    @Column(name = "phone_number",nullable = false )
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private BigDecimal salary;
+}
