dbb4f628cf9df443b3bd676e98fd390ec2ec336b
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-07-28 12:38:50 -0500

    chore: Initial Spring Boot project

---
+++ src/main/java/com/jhoel/empleados_api/EmpleadosApiApplication.java
@@ -0,0 +1,13 @@
+package com.jhoel.empleados_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpleadosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpleadosApiApplication.class, args);
+	}
+
+}
