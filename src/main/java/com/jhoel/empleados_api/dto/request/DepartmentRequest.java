1cb65416260cecf633e16c376ed1f026226e9873
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-07-30 18:21:22 -0500

    refactor: rename DeparmentRequest to DepartmentRequest

--- src/main/java/com/jhoel/empleados_api/dto/request/DepartmentRequest.java
+++ src/main/java/com/jhoel/empleados_api/dto/request/DepartmentRequest.java
@@ -7,7 +7,7 @@ import lombok.*;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class DeparmentRequest {
+public class DepartmentRequest {
     private String name;
     private String officeLocation;
 }
