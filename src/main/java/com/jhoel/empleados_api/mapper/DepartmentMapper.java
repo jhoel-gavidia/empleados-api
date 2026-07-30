919caeca01ab682701ccf8647c028b2be3ac5de8
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-07-30 18:36:08 -0500

    feat: implement DepartmentMapper

--- src/main/java/com/jhoel/empleados_api/mapper/DepartmentMapper.java
+++ src/main/java/com/jhoel/empleados_api/mapper/DepartmentMapper.java
@@ -1,5 +1,27 @@
 package com.jhoel.empleados_api.mapper;
 
 
+import com.jhoel.empleados_api.dto.request.DepartmentRequest;
+import com.jhoel.empleados_api.dto.response.DepartmentResponse;
+import com.jhoel.empleados_api.entity.Department;
+import org.springframework.stereotype.Component;
+
+@Component
 public class DepartmentMapper {
+
+    public Department toEntity(DepartmentRequest departmentRequest) {
+        return Department.builder()
+                .name(departmentRequest.getName())
+                .officeLocation(departmentRequest.getOfficeLocation())
+                .build();
+    }
+
+    public DepartmentResponse toResponse(Department department) {
+        return  DepartmentResponse.builder()
+                .id(department.getId())
+                .name(department.getName())
+                .officeLocation(department.getOfficeLocation())
+                .build();
+    }
+
 }
