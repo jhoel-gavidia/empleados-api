cd4ce90024559034330b96d70d6cb619995f4312
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-07-30 18:43:14 -0500

    feat: implement createDepartment

--- src/main/java/com/jhoel/empleados_api/service/Interfaces/DepartmentService.java
+++ src/main/java/com/jhoel/empleados_api/service/Interfaces/DepartmentService.java
@@ -1,4 +1,9 @@
 package com.jhoel.empleados_api.service.Interfaces;
 
-public class DepartmentService {
+import com.jhoel.empleados_api.dto.request.DepartmentRequest;
+import com.jhoel.empleados_api.dto.response.DepartmentResponse;
+import com.jhoel.empleados_api.entity.Department;
+
+public interface DepartmentService {
+    public DepartmentResponse createDepartment(DepartmentRequest departmentRequest);
 }
