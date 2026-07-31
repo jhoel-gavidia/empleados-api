2c652cd9f824320f53ea2c5207a2d160ce89df91
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-07-30 20:21:04 -0500

    feat: implement create department service

--- src/main/java/com/jhoel/empleados_api/service/Impl/DepartmentImpl.java
+++ src/main/java/com/jhoel/empleados_api/service/Impl/DepartmentImpl.java
@@ -1,4 +1,36 @@
 package com.jhoel.empleados_api.service.Impl;
 
-public class DepartmentImpl {
+import com.jhoel.empleados_api.dto.request.DepartmentRequest;
+import com.jhoel.empleados_api.dto.response.DepartmentResponse;
+import com.jhoel.empleados_api.entity.Department;
+import com.jhoel.empleados_api.exception.AlreadyExistsException;
+import com.jhoel.empleados_api.mapper.DepartmentMapper;
+import com.jhoel.empleados_api.repository.DepartmentRepository;
+import com.jhoel.empleados_api.service.Interfaces.DepartmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class DepartmentImpl implements DepartmentService {
+
+    private final DepartmentRepository departmentRepository;
+    private final DepartmentMapper departmentMapper;
+
+    @Override
+    public DepartmentResponse createDepartment(DepartmentRequest departmentRequest) {
+        boolean exists = departmentRepository.existsByName(departmentRequest.getName());
+
+        if(exists){
+            throw new AlreadyExistsException("Department with name " + departmentRequest.getName() + " already exists");
+        }
+
+        Department department = departmentMapper.toEntity(departmentRequest);
+
+        Department savedDepartment = departmentRepository.save(department);
+
+        return departmentMapper.toResponse(savedDepartment);
+    }
+
+
 }
