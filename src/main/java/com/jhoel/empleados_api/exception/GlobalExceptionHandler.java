f1b76406fdd4a8f001b8cf9da1fa7f09d0b1f53c
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-07-30 20:36:23 -0500

    feat: implement handleValidationErrors

--- src/main/java/com/jhoel/empleados_api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jhoel/empleados_api/exception/GlobalExceptionHandler.java
@@ -2,8 +2,12 @@ package com.jhoel.empleados_api.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class GlobalExceptionHandler {
@@ -19,5 +23,29 @@ public class GlobalExceptionHandler {
         );
 
         return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationErrorResponse> handleValidationErrors(
+            MethodArgumentNotValidException exception
+    ) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        exception.getBindingResult()
+                .getFieldErrors()
+                .forEach(error ->
+                        errors.put(
+                                error.getField(),
+                                error.getDefaultMessage()
+                        )
+                );
+
+        ValidationErrorResponse response = new ValidationErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                errors
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
