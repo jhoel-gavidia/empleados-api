364568893bb6cafb1eab9d778c005374dc10d7b6
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-07-30 20:50:45 -0500

    feat: implement create department endpoint

--- src/main/java/com/jhoel/empleados_api/controller/DepartmentController.java
+++ src/main/java/com/jhoel/empleados_api/controller/DepartmentController.java
@@ -1,4 +1,35 @@
 package com.jhoel.empleados_api.controller;
 
+import com.jhoel.empleados_api.dto.request.DepartmentRequest;
+import com.jhoel.empleados_api.dto.response.DepartmentResponse;
+import com.jhoel.empleados_api.entity.Department;
+import com.jhoel.empleados_api.service.Interfaces.DepartmentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/departments")
+@RequiredArgsConstructor
 public class DepartmentController {
+
+    private final DepartmentService departmentService;
+
+    @PostMapping
+    public ResponseEntity<DepartmentResponse> createDepartment(
+            @Valid @RequestBody DepartmentRequest departmentRequest
+    ) {
+
+        DepartmentResponse response = departmentService.createDepartment(departmentRequest);
+
+        URI location =  URI.create("/api/departments/" + response.getId());
+
+        return ResponseEntity.created(location).body(response);
+    }
 }
