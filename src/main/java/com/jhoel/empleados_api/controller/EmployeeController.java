789b63ba0cadfa1ffda3128b2d77be56f9e69753
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-08-03 18:18:47 -0500

    feat: implement create employee endpoint

--- src/main/java/com/jhoel/empleados_api/controller/EmployeeController.java
+++ src/main/java/com/jhoel/empleados_api/controller/EmployeeController.java
@@ -1,12 +1,33 @@
 package com.jhoel.empleados_api.controller;
 
+import com.jhoel.empleados_api.dto.request.EmployeeRequest;
+import com.jhoel.empleados_api.dto.response.EmployeeResponse;
+import com.jhoel.empleados_api.service.Interfaces.EmployeeService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/employees")
 @RequiredArgsConstructor
 public class EmployeeController {
 
+    private final EmployeeService employeeService;
+
+    @PostMapping
+    public ResponseEntity<EmployeeResponse> createEmployee(
+            @Valid @RequestBody EmployeeRequest employeeRequest) {
+
+        EmployeeResponse response = employeeService.createEmployee(employeeRequest);
+
+        URI location = URI.create("/api/employees/" + response.getId());
+
+        return ResponseEntity.created(location).body(response);
+    }
 }
