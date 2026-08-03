1fe7562c6f532c525c3ea8d7d8d4c6940314a66b
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-08-03 17:57:49 -0500

    feat: add get employees

--- src/main/java/com/jhoel/empleados_api/service/Interfaces/EmployeeService.java
+++ src/main/java/com/jhoel/empleados_api/service/Interfaces/EmployeeService.java
@@ -2,8 +2,12 @@ package com.jhoel.empleados_api.service.Interfaces;
 
 import com.jhoel.empleados_api.dto.request.EmployeeRequest;
 import com.jhoel.empleados_api.dto.response.EmployeeResponse;
+import com.jhoel.empleados_api.entity.Employee;
+
+import java.util.List;
 
 public interface EmployeeService {
     EmployeeResponse createEmployee(EmployeeRequest employeeRequest);
     EmployeeResponse getEmployeeById(Long id);
+    List<Employee> getEmployees();
 }
