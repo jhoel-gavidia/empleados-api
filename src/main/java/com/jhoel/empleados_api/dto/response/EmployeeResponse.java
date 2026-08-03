0654f753aaa653fcd0f43de6d9019cb2629f1645
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-08-03 17:26:44 -0500

    feat: implement employee response

--- src/main/java/com/jhoel/empleados_api/dto/response/EmployeeResponse.java
+++ src/main/java/com/jhoel/empleados_api/dto/response/EmployeeResponse.java
@@ -1,4 +1,25 @@
 package com.jhoel.empleados_api.dto.response;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
 public class EmployeeResponse {
+
+    private final Long id;
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    private final LocalDate birthDate;
+    private final String phoneNumber;
+    private final BigDecimal salary;
+    private final Long departmentId;
+    private final String departmentName;
+
 }
