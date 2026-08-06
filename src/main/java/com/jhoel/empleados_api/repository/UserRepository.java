c5bb7a65d21e2c2b442082791ce5dccb4a4e2335
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-08-06 15:57:44 -0500

    feat: add query for user authentication

--- src/main/java/com/jhoel/empleados_api/repository/UserRepository.java
+++ src/main/java/com/jhoel/empleados_api/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.jhoel.empleados_api.repository;
 import com.jhoel.empleados_api.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
