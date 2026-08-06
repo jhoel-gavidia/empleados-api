253eee44fa93cbc2a49c2542935362bdfe8bdd02
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-08-06 16:29:44 -0500

    chore(repository): update query methods to use @Query

--- src/main/java/com/jhoel/empleados_api/repository/UserRepository.java
+++ src/main/java/com/jhoel/empleados_api/repository/UserRepository.java
@@ -2,9 +2,18 @@ package com.jhoel.empleados_api.repository;
 
 import com.jhoel.empleados_api.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
+
+    @Query("""
+    Select u
+    from User u
+    Where u.username = :identifier
+    Or u.email = :identifier
+    """)
+    Optional<User> findByUsernameOrEmail(@Param("indentifier") String identifier);
 }
