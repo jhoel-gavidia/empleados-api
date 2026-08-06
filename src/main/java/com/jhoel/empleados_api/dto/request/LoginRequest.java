6593c07e09c489cfde455414d9dc40012f456bdd
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-08-06 16:31:08 -0500

    feat: implement login request

---
+++ src/main/java/com/jhoel/empleados_api/dto/request/LoginRequest.java
@@ -0,0 +1,21 @@
+package com.jhoel.empleados_api.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+
+    @NotBlank(message = "Username or email is required")
+    private String usernameOrEmail;
+
+    @NotBlank(message = "Password is requerid")
+    private String password;
+
+}
