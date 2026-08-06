87ef2bd172c1d4822bf0fa8da4f2c1407ab48779
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-08-06 15:42:13 -0500

    feat: add entities annotations

--- src/main/java/com/jhoel/empleados_api/entity/User.java
+++ src/main/java/com/jhoel/empleados_api/entity/User.java
@@ -1,19 +1,22 @@
 package com.jhoel.empleados_api.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "user_name", nullable = false, unique = true)
-    private String userName;
+    private String username;
 
     @Column(nullable = false, unique = true)
     private String email;
 
+    @Column(nullable = false)
     private String password;
 
     @Enumerated(EnumType.STRING)
