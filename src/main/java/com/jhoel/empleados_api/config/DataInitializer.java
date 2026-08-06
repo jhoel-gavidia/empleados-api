57f2c876e40da4ed5c8764f1c0faca7a076b555f
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-08-06 16:43:04 -0500

    feat: add data initializer

---
+++ src/main/java/com/jhoel/empleados_api/config/DataInitializer.java
@@ -0,0 +1,33 @@
+package com.jhoel.empleados_api.config;
+
+import com.jhoel.empleados_api.entity.Role;
+import com.jhoel.empleados_api.entity.User;
+import com.jhoel.empleados_api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class DataInitializer {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Bean
+    CommandLineRunner initDatabase() {
+        return  args -> {
+            if(userRepository.findByUsernameOrEmail("email").isEmpty()) {
+                User admin = User.builder()
+                        .username("admin")
+                        .password("admin123")
+                        .role(Role.ADMIN)
+                        .build();
+
+                userRepository.save(admin);
+            }
+        };
+    }
+}
