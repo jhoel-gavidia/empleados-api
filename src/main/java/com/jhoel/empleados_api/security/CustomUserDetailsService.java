0577f0d6a383474fc1e216526a5494b8412ca36e
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-08-06 16:00:41 -0500

    feat: implement user details service

---
+++ src/main/java/com/jhoel/empleados_api/security/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.jhoel.empleados_api.security;
+
+import com.jhoel.empleados_api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(
+                        () -> new UsernameNotFoundException("User not found with username: " + username)
+                );
+    }
+}
