6169a6cdb40ea5791cd38eef055105a9be463a4d
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-08-06 20:11:16 -0500

    feat: implements extract all claims

--- src/main/java/com/jhoel/empleados_api/security/JwtService.java
+++ src/main/java/com/jhoel/empleados_api/security/JwtService.java
@@ -1,5 +1,6 @@
 package com.jhoel.empleados_api.security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
@@ -35,10 +36,19 @@ public class JwtService {
                 .compact();
     }
 
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
     private SecretKey getKey() {
         return  Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
     }
 
     public String extractUsername(String jwt) {
+        return
     }
 }
