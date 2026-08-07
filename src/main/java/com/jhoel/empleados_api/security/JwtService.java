ea99ec4574cd088844cffeb6870a121ea8c50800
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-08-06 20:06:34 -0500

    feat: implement jwt service

---
+++ src/main/java/com/jhoel/empleados_api/security/JwtService.java
@@ -0,0 +1,41 @@
+package com.jhoel.empleados_api.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import com.jhoel.empleados_api.entity.User;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    public String generateToken(User user) {
+        Map<String, Object> claims = new HashMap<String, Object>();
+
+        claims.put("role", user.getUsername());
+
+        return Jwts.builder()
+                .claims(claims)
+                .subject(user.getUsername())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getKey())
+                .compact();
+    }
+
+    private SecretKey getKey() {
+        return  Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
+}
