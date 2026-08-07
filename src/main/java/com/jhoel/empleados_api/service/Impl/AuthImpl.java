884e641eb43069e415eb0258086a4bcb7ecd81d1
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-08-06 20:07:05 -0500

    feat: add method generateToken in auth service

--- src/main/java/com/jhoel/empleados_api/service/Impl/AuthImpl.java
+++ src/main/java/com/jhoel/empleados_api/service/Impl/AuthImpl.java
@@ -3,6 +3,7 @@ package com.jhoel.empleados_api.service.Impl;
 import com.jhoel.empleados_api.dto.request.LoginRequest;
 import com.jhoel.empleados_api.dto.response.AuthResponse;
 import com.jhoel.empleados_api.entity.User;
+import com.jhoel.empleados_api.security.JwtService;
 import com.jhoel.empleados_api.service.Interfaces.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Service;
 public class AuthImpl implements AuthService {
 
     private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
     @Override
     public AuthResponse login(LoginRequest loginRequest) {
@@ -26,7 +28,10 @@ public class AuthImpl implements AuthService {
 
         User user = (User) authentication.getPrincipal();
 
+        String token = jwtService.generateToken(user);
+
         return AuthResponse.builder()
+                .token(token)
                 .username(user.getUsername())
                 .role(user.getRole())
                 .build();
