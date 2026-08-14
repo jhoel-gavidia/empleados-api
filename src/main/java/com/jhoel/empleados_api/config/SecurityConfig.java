d7480df6405cd96c3902acccbd30c0bd64058946
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-08-13 23:05:25 -0500

    feat: add actuator health checks

--- src/main/java/com/jhoel/empleados_api/config/SecurityConfig.java
+++ src/main/java/com/jhoel/empleados_api/config/SecurityConfig.java
@@ -63,7 +63,8 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/api/auth/**",
                                 "/swagger-ui/**",
-                                "/v3/api-docs/**"
+                                "/v3/api-docs/**",
+                                "/actuator/health"
                         )
                         .permitAll()
 
